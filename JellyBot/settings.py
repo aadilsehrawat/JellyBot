8d608aa47669b07cfafea519c4f9d89be5978041
unknown <aadilsehrawat@gmail.com>
2023-04-22 20:28:49 +0530

    Reverted settings temporarily

--- JellyBot/settings.py
+++ JellyBot/settings.py
@@ -12,22 +12,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / 'secrets.env')
+# load_dotenv(BASE_DIR / 'secrets.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'random_string')
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'random_string')
+SECRET_KEY = 'django-insecure-_wp3@=f_rv(&!3(li71ld&7uvfou*9+7rg_fxgg7%y%t39*g0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False')
+# DEBUG = os.environ.get('DEBUG', 'False')
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.0.198' , '.vercel.app', '*']
 
