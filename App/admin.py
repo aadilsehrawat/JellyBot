50d656c2ea8a8d34b6dc1f42204a51d64752488f
unknown <aadilsehrawat@gmail.com>
2023-04-22 18:51:22 +0530

    Added models admins for -> Actions, ContactUs and PerformLogs

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('action', 'description', 'performed')
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
+
+class PerformLogsAdmin(admin.ModelAdmin):
+    list_display = ('action', 'input_file', 'output_file')
+
+admin.site.register(Actions, ActionAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
+admin.site.register(PerformLogs, PerformLogsAdmin)
