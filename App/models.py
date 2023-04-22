e66ab85207a069c52c64e5a7b61720c642fbf61b
unknown <aadilsehrawat@gmail.com>
2023-04-22 18:51:07 +0530

    Added models -> Actions, ContactUs and PerformLogs

--- App/models.py
+++ App/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Actions(models.Model):
+    action = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    performed = models.IntegerField(default=0)
+    cover = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Action'
+        verbose_name_plural = 'Actions'
+    
+    def __str__(self):
+        return self.action 
+    
+class ContactUs(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = "Contact Us"
+        verbose_name_plural = "Contact Us"
+
+    def __str__(self):
+        return str(self.name) + str(self.email)
+    
+class PerformLogs(models.Model):
+    action = models.ForeignKey(Actions, on_delete=models.CASCADE)
+    input_file = models.FileField(upload_to='input_file/', blank=True, null=True)
+    output_file = models.FileField(upload_to='output_file/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Perform Logs"
+        verbose_name_plural = "Perform Logs"
+
+    def __str__(self):
+        return str(self.action)
