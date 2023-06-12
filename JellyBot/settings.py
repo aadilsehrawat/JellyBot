ee9ba640d7a1c8f0d58435f8998a1b4a25682a3c
aadilsehrawat <aadilsehrawat@gmail.com>
2023-06-12 20:04:19 +0530

    Reverted back static settigns

--- JellyBot/settings.py
+++ JellyBot/settings.py
@@ -138,9 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATIC_DIRS = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
+STATIC_ROOT = '/static/'
 MEDIA_ROOT = '/media/'
 
 if os.environ.get('VERCEL_ENV') == 'production':
