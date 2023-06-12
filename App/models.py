51cf56b3ba8145dddba776f3e80f2a07cd966d98
aadilsehrawat <aadilsehrawat@gmail.com>
2023-06-12 20:38:54 +0530

    Updated CharField to TextField in ActionSteps and Acitons.Description

--- App/models.py
+++ App/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Actions(models.Model):
     action_order = models.IntegerField(default=1, unique=True)
     action = models.CharField(max_length=100)
-    description = models.CharField(max_length=100, default='No description provided yet!')
+    description = models.TextField(default='No description provided yet!')
     times_performed = models.IntegerField(default=0)
     cover_image = models.FileField(upload_to='media/action_cover_images/', blank=True, null=True)
     cover_image_url = models.CharField(max_length=100, blank=True, null=True)
@@ -20,11 +20,11 @@ class Actions(models.Model):
     
 class ActionSteps(models.Model):
     action = models.ForeignKey(Actions, on_delete=models.CASCADE)
-    step1 = models.CharField(max_length=100)
-    step2 = models.CharField(max_length=100)
-    step3 = models.CharField(max_length=100)
-    step4 = models.CharField(max_length=100)
-    step5 = models.CharField(max_length=100)
+    step1 = models.TextField(default="Step 1")
+    step2 = models.TextField(default="Step 2")
+    step3 = models.TextField(default="Step 3")
+    step4 = models.TextField(default="Step 4")
+    step5 = models.TextField(default="Step 5")
 
     class Meta:
         verbose_name = 'Action Step'
@@ -41,15 +41,3 @@ class ContactUs(models.Model):
 
     def __str__(self):
         return str(self.name) + ' ' + str(self.email)
-    
-# class PerformLogs(models.Model):
-#     action = models.ForeignKey(Actions, on_delete=models.CASCADE)
-#     input_file = models.FileField(upload_to='action_input_file/', blank=True, null=True)
-#     output_file = models.FileField(upload_to='action_output_file/', blank=True, null=True)
-
-#     class Meta:
-#         verbose_name = "Perform Logs"
-#         verbose_name_plural = "Perform Logs"
-
-#     def __str__(self):
-#         return str(self.action)
